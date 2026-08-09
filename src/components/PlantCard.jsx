edb5f307fbdecfc4ab096410e78578c53cd262c5
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-09 16:08:06 +0300

    added a button to show if the plant is soldout or no

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
+  const [soldOut, setSoldOut] = useState(false);
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h2>{plant.name}</h2>
       <p>Price: ${plant.price}</p>
-      <button>In Stock</button>
+
+      <button onClick={() => setSoldOut(true)}>
+        {soldOut ? "Sold Out" : "In Stock"}
+      </button>
     </li>
   );
 }
